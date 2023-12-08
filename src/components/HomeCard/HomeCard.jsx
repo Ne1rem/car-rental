a9b4aabb65fff0c14a0937d3099233e3835ec62e
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-12-08 14:22:14 +0200

    і

---
+++ src/components/HomeCard/HomeCard.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import css from '../../pages/Home/Home.module.css';
+
+
+const HomeCard = ({
+  id,
+  img,
+  photoLink,
+  make,
+  model,
+  year,
+  rentalPrice,
+  address,
+  type,
+  mileage,
+  rentalCompany,
+  openModalHandler,
+}) => {
+
+
+  return (
+    <>
+    <div className={css.HomeDivText}>
+      <div className={css.HomeimageWrapper}>
+        <img src={!img ? photoLink : img} alt="car_image" className={css.HomecarImage} />
+      </div>
+
+      <div className={css.Hometitle}>
+        <p>
+          {make}
+          <span className={css.Homemodel}>{model},</span>
+          <span className={css.Homeyear}>{year}</span>
+        </p>
+        <p className={css.Homeprice}>{rentalPrice}</p>
+      </div>
+      <div className={css.Hometags}>
+        <p>
+          {address.split(',')[1]} <span> | </span>
+          {address.split(',')[2]}
+          <span> | </span>
+          {rentalCompany}
+          <span> | </span>
+          {type}
+          <span> | </span>
+          {model}
+          <span> | </span>
+          {id}
+          <span> | </span>
+          {mileage}
+        </p>
+      </div>
+      <button
+        type="button"
+        className={css.HomelearnMoreBtn}
+        onClick={openModalHandler}
+        id={id}
+      >
+        Learn more
+      </button>
+      </div>
+    </>
+  );
+};
+
+export default HomeCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 import css from './Home.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { setActivePage, setModal, setModalCar } from 'redux/cars-slice';
-import CarCard from 'components/CarCard/CarCard';
 import carsSelectors from 'redux/cars-selectors';
 import Modal from 'components/Modal/Modal';
 import Notiflix from 'notiflix';
+import HomeCard from 'components/HomeCard/HomeCard';
 
 const Home = () => {
   const TEL_NUMBER = process.env.REACT_APP_TEL;
@@ -16,6 +16,7 @@ const Home = () => {
   const [opacity1, setOpacity1] = useState(1);
   const [opacity2, setOpacity2] = useState(0);
   const [opacity3, setOpacity3] = useState(0);
+  const [opacity4, setOpacity4] = useState(0);
 
   const [show, setShow] = useState(1);
   const [margin, setMargin] = useState(window.innerWidth / 3.3334 - 127);
@@ -30,14 +31,22 @@ const Home = () => {
       if (show === 1) {
         setOpacity1(0);
         setOpacity2(1);
+        setOpacity3(2)
         setShow(2);
       } else if (show === 2) {
         setOpacity2(0);
         setOpacity3(1);
+        setOpacity4(2)
         setShow(3);
-      } else {
-        setOpacity1(1);
+      } else if (show === 2) {
         setOpacity3(0);
+        setOpacity4(1);
+        setOpacity1(2)
+        setShow(4);
+      } else {
+        setOpacity4(0);
+        setOpacity1(1);
+        setOpacity2(2)
         setShow(1);
       }
     }
@@ -46,69 +55,73 @@ const Home = () => {
     return () => clearInterval(intervalId);
   }, [dispatch, show]);
 
-    const galleryCars = useSelector(carsSelectors.getFilteredCars);
+  const galleryCars = useSelector(carsSelectors.getFilteredCars);
 
-    const openModalHandler = e => {
-      dispatch(setModalCar(e.target.id));
-      dispatch(setModal(true));
-  
-      document.body.style.overflow = 'hidden';
-      window.addEventListener('keydown', escHandler);
-    };
-  
-    const closeModalHandler = e => {
-      if (e.target.id === 'overlay' || e.currentTarget.id === 'closeModal') {
-        dispatch(setModal(false));
-        window.removeEventListener('keydown', escHandler);
-      }
-      document.body.style.overflow = 'unset';
-    };
-  
-    const escHandler = e => {
-      if (e.code === 'Escape') {
-        dispatch(setModal(false));
-        window.removeEventListener('keydown', escHandler);
-      }
-    };
+  const openModalHandler = (e) => {
+    dispatch(setModalCar(e.target.id));
+    dispatch(setModal(true));
 
-    const rentalCarHandler = () => {
-      Notiflix.Notify.init({
-        zindex: 9999999,
-      });
-      Notiflix.Notify.success('Contact us!');
-    };
+    document.body.style.overflow = 'hidden';
+    window.addEventListener('keydown', escHandler);
+  };
+
+  const closeModalHandler = (e) => {
+    if (e.target.id === 'overlay' || e.currentTarget.id === 'closeModal') {
+      dispatch(setModal(false));
+      window.removeEventListener('keydown', escHandler);
+    }
+    document.body.style.overflow = 'unset';
+  };
+
+  const escHandler = (e) => {
+    if (e.code === 'Escape') {
+      dispatch(setModal(false));
+      window.removeEventListener('keydown', escHandler);
+    }
+  };
+
+  const rentalCarHandler = () => {
+    Notiflix.Notify.init({
+      zindex: 9999999,
+    });
+    Notiflix.Notify.success('Contact us!');
+  };
 
   return (
     <>
-    {isModalOn && (
+      {isModalOn && (
         <Modal
           closeModalHandler={closeModalHandler}
           rentalCarHandler={rentalCarHandler}
         />
       )}
       <section className={css.section}>
-      <p className={css.heroBest}>Best Rental Cars</p>
-      {galleryCars.map(car => {
-              return (
-                <li className={css.carCard} key={car.id}>
-                  <CarCard
-                    id={car.id}
-                    img={car.img}
-                    photoLink={car.photoLink}
-                    make={car.make}
-                    model={car.model}
-                    year={car.year}
-                    rentalPrice={car.rentalPrice} 
-                    address={car.address}
-                    type={car.type}
-                    functionalities={car.functionalities}
-                    mileage={car.mileage}
-                    rentalCompany={car.rentalCompany}
-                    openModalHandler={openModalHandler}
-                  />
-                </li>
-              );
-            })}
+        <p className={css.heroBest}>Best Rental Cars</p>
+        {galleryCars
+          .filter((car) => parseFloat(car.rentalPrice.substring(1)) > 200)
+          .map((car) => {
+            const rentalPrice = parseFloat(car.rentalPrice.substring(1));
+
+            return (
+              <li className={css.homeCard} key={car.id}>
+                <HomeCard
+                  id={car.id}
+                  img={car.img}
+                  photoLink={car.photoLink}
+                  make={car.make}
+                  model={car.model}
+                  year={car.year}
+                  rentalPrice={rentalPrice}
+                  address={car.address}
+                  type={car.type}
+                  functionalities={car.functionalities}
+                  mileage={car.mileage}
+                  rentalCompany={car.rentalCompany}
+                  openModalHandler={openModalHandler}
+                />
+              </li>
+            );
+          })}
       </section>
       <div className={css.info} style={{ marginTop: margin }}>
         <h1>DriveEasy Rental</h1>
