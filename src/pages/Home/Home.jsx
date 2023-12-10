d07c4a1ccbe38050e8d50aa7f864260b0a2dc222
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-12-10 18:42:52 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,73 +1,65 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './Home.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { setActivePage, setModal, setModalCar } from 'redux/cars-slice';
+import { useSelector } from 'react-redux';
 import carsSelectors from 'redux/cars-selectors';
-import Modal from 'components/Modal/Modal';
-import Notiflix from 'notiflix';
 import HomeCard from 'components/HomeCard/HomeCard';
 
 const Home = () => {
-  const TEL_NUMBER = process.env.REACT_APP_TEL;
-
-  const isModalOn = useSelector(carsSelectors.getModalOn);
-
-  const [opacity1, setOpacity1] = useState(1);
-  const [opacity2, setOpacity2] = useState(0);
-  const [opacity3, setOpacity3] = useState(0);
-  const [opacity4, setOpacity4] = useState(0);
-
   const [show, setShow] = useState(1);
-  const [margin, setMargin] = useState(window.innerWidth / 3.3334 - 127);
-  const dispatch = useDispatch();
+  const [opacity, setOpacity] = useState({
+    opacity1: 1,
+    opacity2: 0,
+    opacity3: 0,
+    opacity4: 0,
+  });
+  const galleryCars = useSelector(carsSelectors.getFilteredCars);
 
   useEffect(() => {
-    setMargin(window.innerWidth / 3.3334 - 127);
-
-    dispatch(setActivePage('home'));
-
     function heroFun() {
+      const newOpacity = { ...opacity };
+
       if (show === 1) {
-        setOpacity1(1);
-        setOpacity2(0);
-        setOpacity3(0);
-        setOpacity4(0);
+        newOpacity.opacity1 = 1;
+        newOpacity.opacity2 = 0;
+        newOpacity.opacity3 = 0;
+        newOpacity.opacity4 = 0;
         setShow(2);
       } else if (show === 2) {
-        setOpacity1(0);
-        setOpacity2(1);
-        setOpacity3(0);
-        setOpacity4(0);
+        newOpacity.opacity1 = 0;
+        newOpacity.opacity2 = 1;
+        newOpacity.opacity3 = 0;
+        newOpacity.opacity4 = 0;
         setShow(3);
       } else if (show === 3) {
-        setOpacity1(0);
-        setOpacity2(0);
-        setOpacity3(1);
-        setOpacity4(0);
+        newOpacity.opacity1 = 0;
+        newOpacity.opacity2 = 0;
+        newOpacity.opacity3 = 1;
+        newOpacity.opacity4 = 0;
         setShow(4);
       } else {
-        setOpacity1(0);
-        setOpacity2(0);
-        setOpacity3(0);
-        setOpacity4(1);
+        newOpacity.opacity1 = 0;
+        newOpacity.opacity2 = 0;
+        newOpacity.opacity3 = 0;
+        newOpacity.opacity4 = 1;
         setShow(1);
       }
+
+      setOpacity(newOpacity);
     }
-    let intervalId = setInterval(() => heroFun(), 5000);
+
+    let intervalId = setInterval(() => heroFun(), 4000);
 
     return () => clearInterval(intervalId);
-  }, [show]);
-
-  const galleryCars = useSelector(carsSelectors.getFilteredCars);
+  }, [show, opacity]);
 
   return (
     <>
       <section className={css.section}>
-      <div className={css.info}>
-        <h1>Uk Rental Car</h1>
-        <h2>The best rental car service in Ukraine </h2>
-      </div>
+        <div className={css.info}>
+          <h1>Uk Rental Car</h1>
+          <h2>The best rental car service in Ukraine</h2>
+        </div>
         <ul className={css.homeCardList}>
           {galleryCars
             .filter(car => parseFloat(car.rentalPrice.substring(1)) > 200)
@@ -76,7 +68,7 @@ const Home = () => {
               const opacity = show === index + 1 ? 1 : 0;
 
               return (
-                <li key={car.id} className={css.homeCard} style={{ opacity }}>
+                <li key={car.id} className={css.homeCard} style={{ opacity: opacity }}>
                   <HomeCard
                     id={car.id}
                     img={car.img}
@@ -102,9 +94,12 @@ const Home = () => {
             </NavLink>
           </div>
         </ul>
-        
       </section>
-      <p className={css.heroBest}>Our UK Rental Car company is a dynamically developing company, one of the largest in Kyiv, given the number of vehicle collection/delivery points.</p>
+      <p className={css.heroBest}>
+        Our UK Rental Car company is a dynamically developing company, one of
+        the largest in Kyiv, given the number of vehicle collection/delivery
+        points.
+      </p>
     </>
   );
 };
