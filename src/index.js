4b422db732cb4ca826350a4901b53183bde24e39
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-12-10 18:27:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/car-rental-service">
+        <BrowserRouter basename="/car-rental">
           <App />
         </BrowserRouter>
       </PersistGate>
